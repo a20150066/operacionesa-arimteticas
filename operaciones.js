8d8e88ec1318f3baf79f56d4a6b542640a457bb4
a20150066 <a20150066@juan23.edu.pe>
2024-11-04 13:18:27 -0500

    Add files via upload

---
+++ operaciones.js
@@ -0,0 +1,24 @@
+document.write("<h1>Operaciones Básicas</h1>");
+
+var num1 = parseInt(prompt("Ingresa primer número:"));
+var num2 = parseInt(prompt("Ingresa segundo número:"));
+
+var sum = num1 + num2;
+var res = num1 - num2;
+var mul = num1 * num2;
+var div = num1 / num2;
+
+document.write("El primer número fue: " + num1);
+document.write("<br>");
+document.write("El primer número fue: " + num2);
+document.write("<br>");
+document.write("La suma de ambos es:" + sum);
+document.write("<br>");
+document.write("La resta de ambos es:" + res);
+document.write("<br>");
+document.write("El producto de ambos es:" + mul);
+document.write("<br>");
+document.write("La división de ambos es:" + div);
+document.write("<br>");
+document.write("<br>");
+document.write("<marquee>Hecho por Alessandra Vargas</marquee>");
